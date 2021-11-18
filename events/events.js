c2efa7baadddfb54757589227131a0e495cb57d2
Quilist <93136218+Quilist@users.noreply.github.com>
2021-11-18 13:16:47 +0200

    Update events.js

--- events/events.js
+++ events/events.js
@@ -1,6 +1,7 @@
 const cache = require('./cache.json');
 const fs = require('fs');
 const Events = require('events');
+const request = require('request');
 
 class EventHandler extends Events.EventEmitter {
     constructor(client) {
@@ -10,13 +11,28 @@ class EventHandler extends Events.EventEmitter {
     }
 
     async set_callbacks() {
-        const updates = this.client.history();
+
+        const updates = () => {
+            return new Promise((resolve, reject) => {
+                request.post('https://payeer.com/ajax/api/api.php?history', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    body: `account=${this.client.account}&apiId=${this.client.api_id}&apiPass=${this.client.api_pass}&action=history`
+                }, (error, response, body) => {
+                    if (error) reject(error)
+                    else resolve(JSON.parse(body))
+                });
+            })
+        }
 
         const callback = () => {
-            updates.then((data) => {
+            updates().then((data) => {
+
                 const array = new Array();
                 const history = data.history;
-                const keys = Object.keys(data.history);
+                const keys = Object.keys(history);
                 const last_element = history[keys[keys.length - 1]]
 
                 if (cache.history.id !== last_element.id) {
